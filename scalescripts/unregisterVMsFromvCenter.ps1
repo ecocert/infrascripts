4e3cd34a4538a9938966cacc49bf3c4f61d0e483
ecocert <34893807+ecocert@users.noreply.github.com>
2018-01-18 21:52:39 +0530

    Add files via upload
    Powercli scripts to register and unregister VMs

---
+++ scalescripts/unregisterVMsFromvCenter.ps1
@@ -0,0 +1,26 @@
+﻿param($start,$end) 
+#####################################################################
+# Load VMware Plugins and connect to vCenter
+#####################################################################
+ 
+Add-PSSnapin vmware.vimautomation.core
+## Enter your vCenter here
+connect-viserver -server vcsa-02a.corp.local -User administrator@corp.local -Password VMware1!
+ 
+
+Function unregistervm([int] $StartNumOfVM, [int] $endNumOfVM)
+{
+  For ( $num=$StartNumOfVM;$num -le $endNumOfVM;$num++) { 
+    $vm = get-VM Tiny-Linux-VM-$num
+    
+    Remove-VM -VM $vm -DeleteFromDisk:$false -Confirm:$false -RunAsync
+    Write-Host -ForegroundColor GREEN "Unregistering VM: $vm"
+ }
+}
+ $time = Get-Date
+ write-Host "Begin time $time"
+ unregistervm  $start $end
+ $time = Get-Date
+ Write-Host "End time $time"
+
+Disconnect-VIServer -server vcsa-02a.corp.local -confirm:$false
